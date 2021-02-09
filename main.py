adfeb6ec42896a40f1ba251616a1db71581a9080
gholamifarshad98 <gholamifarshad98@yahoo.com>
2021-02-09 10:00:38 +0330

    in this edition i have blur left and right image and after that i have calculte changes in brightness and after that apply xor for those result by specfic thresold.

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-from meshCreator import MeshCreator
 import matplotlib.pyplot as plt
 from builtins import range
 import numpy
 import os
 import cv2
+
+
+def compare_two_images(ref_image, blur_image, result, ref_height, ref_width):
+    for row in range(ref_height):
+        for col in range(ref_width):
+            result[row][col] = result[row][col] + abs(int(ref_image[row][col]) - int(blur_image[row][col]))
+    return result
+
+
+def compare_images(ref_image, images):
+    image_differentiate = []
+    ref_height, ref_width = ref_image.shape
+    print(ref_height, ref_width)
+    for row in range(ref_height):
+        temp_row = []
+        for col in range(ref_width):
+            temp_row.append(0)
+        image_differentiate.append(temp_row)
+    counter = 0
+    for blur_image in images:
+        print("Image {} is processed".format(counter))
+        counter += 1
+        image_differentiate = compare_two_images(ref_image, blur_image, image_differentiate, ref_height, ref_width)
+    convert_result = numpy.array(image_differentiate)
+    normalized_data = (255*(convert_result - numpy.min(convert_result))/numpy.ptp(convert_result)).astype(numpy.uint8)
+    return normalized_data
+
+
+def xor_results(first_input, second_input, selected_threshold):
+    result = []
+    for row in range(len(first_input)):
+        temp_result = []
+        for col in range(len(first_input[0])):
+            if abs(first_input[row][col] - second_input[row][col]) > int(selected_threshold):
+                temp_result.append(int(255))
+            else:
+                temp_result.append(int(0))
+        result.append(temp_result)
+    numpy_result = numpy.array(result)
+    normalized_data = (255 * (numpy_result - numpy.min(numpy_result)) / numpy.ptp(numpy_result)).astype(numpy.uint8)
+    cv2.imwrite('opencv_gray_{}.png'.format(selected_threshold), normalized_data)
+    cv2.imshow("xor", normalized_data)
+    cv2.waitKey(1000)
+
 
 LeftImage = cv2.imread("data/im0.png", cv2.IMREAD_GRAYSCALE)
 RightImage = cv2.imread("data/im1.png", cv2.IMREAD_GRAYSCALE)
@@ -12,9 +55,6 @@ height = 480
 desireSize = (width, height)
 LeftImage = cv2.resize(LeftImage, desireSize)
 RightImage = cv2.resize(RightImage, desireSize)
-
-
-
 
 ParentPath = os.curdir
 paths = []
@@ -35,72 +75,19 @@ BlurImages = []
 apply_canny_array_on_blur = []
 apply_canny_array = []
 selectedThreshold = 80
+rightImages = []
+leftImages = []
 # in this part we reduce the quality of image by reduce size and change those image to 640*480
 for percentage in numpy.arange(0.1, 1.05, 0.01):
-    tempName1 = "BlurImage_percentage_size{}.png".format(int(percentage*100))
-    fileAddress1 = os.path.join(paths[0], tempName1)
-    tempName2 = "BlurImage_percentage_size_canny{}.png".format(int(percentage*100))
-    fileAddress2 = os.path.join(paths[1], tempName2)
     tempSize = (int(percentage * desireSize[0]), int(percentage * desireSize[1]))
-    firstStep = cv2.resize(LeftImage, tempSize)
-    secondStep = cv2.resize(firstStep, desireSize)
-    BlurImages.append(secondStep)
-    cv2.imshow("BlurImages image", secondStep)
-    cv2.imwrite(fileAddress1,secondStep)
-    thirdStep = cv2.Canny(secondStep, selectedThreshold, selectedThreshold)
-    apply_canny_array_on_blur.append(thirdStep)
-    cv2.imshow("resized image", thirdStep)
-    cv2.imwrite(fileAddress2,thirdStep)
-    cv2.waitKey(300)
+    leftTemp = cv2.resize(LeftImage, tempSize)
+    rightTemp = cv2.resize(RightImage, tempSize)
+    leftImages.append(cv2.resize(leftTemp, desireSize))
+    rightImages.append(cv2.resize(rightTemp, desireSize))
 
-# In this part we change the threshold of canny algorithm
-for threshold in range(20, 200, 5):
-    LeftImageApplyCanny = cv2.Canny(LeftImage, threshold, threshold)
-    tempName = "Threshold{}.png".format(threshold)
-    fileAddress = os.path.join(paths[2], tempName)
-    apply_canny_array.append(LeftImageApplyCanny)
-    step.append(threshold)
-    cv2.imwrite(fileAddress, LeftImageApplyCanny)
-    cv2.imshow("changing threshold", LeftImageApplyCanny)
-    cv2.waitKey(300)
 
-# in this part we mesh 640*480 to grid size (grid_width,grid_height) and after that
-# we count number of detected corner in canny algorithm in each grid for each threshold.
-grid_width = 80
-grid_height = 80
-MyMeshCreator = MeshCreator(desireSize[1], desireSize[0], grid_width, grid_height)
-meshedImage = MyMeshCreator.draw_mesh(LeftImage)
-cv2.imshow("mesh",meshedImage)
-cv2.waitKey(100)
-results = dict()
-i = 0
-
-for image in apply_canny_array:
-    print(i)
-    results[i] = list(MyMeshCreator.get_white_counts_on_image(image))
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    i = i + 1
-# In this part we count ratio of decrease in number of detected corner
-# in canny algorithm in each grid for each threshold.
-accumulatedWhitePixel = []
-differentiateOfWhite = []
-diffStep = []
-grid_row = 4
-grid_col = 2
-for stepInThreshold in results:
-    accumulatedWhitePixel.append(results[stepInThreshold][grid_row][grid_col])
-for i in range(len(step)-1):
-    diffStep.append(step[i])
-    differentiateOfWhite.append(-accumulatedWhitePixel[i+1]+accumulatedWhitePixel[i])
-fig = plt.figure()
-ax1 = fig.add_subplot(121)
-plt.title('Grid index is [{},{}]'.format(grid_row,grid_col))
-ax2 = fig.add_subplot(122)
-plt.title('Grid index is [{},{}]'.format(grid_row,grid_col))
-ax1.plot(step, accumulatedWhitePixel)
-ax2.plot(diffStep, differentiateOfWhite)
-ax1.set_xlabel('threshold')
-ax1.set_ylabel('Number of White pixel')
-ax2.set_xlabel('threshold')
-ax2.set_ylabel('Rate of decrease in number of White pixel')
-plt.show()
+leftImage_differentiate = compare_images(LeftImage, leftImages)
+cv2.imwrite("efe.png",leftImage_differentiate)
+rightImage_differentiate = compare_images(RightImage, rightImages)
+for threshold in range (0,200,10):
+    xor_results(leftImage_differentiate, rightImage_differentiate, threshold)
